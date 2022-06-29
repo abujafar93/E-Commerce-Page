164b6bd41a7c4278269b4bc1503e494caa18eabc
Abdulfatai Quadri Adewale <75619825+abujafar93@users.noreply.github.com>
2022-06-29 15:21:28 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const hambMenu = document.querySelector('#btn1');
 const hambClose = document.querySelector('#btn2');
 const hambDropDownDetails = document.querySelector('.list');
+const slides = document.querySelector('.slideImages')
+const previousKey = document.querySelector('#previous');
+const nextKey = document.querySelector('#next');
 
+
+// HAMBURGER MENU
 hambMenu.addEventListener('click', () => {
   hambMenu.style.display = 'none';
   hambClose.style.display = 'inline';
@@ -12,4 +17,14 @@ hambClose.addEventListener('click', ()=>{
   hambMenu.style.display = 'inline';
   hambClose.style.display = 'none';
   hambDropDownDetails.style.display = 'none';
+})
+
+
+// SLIDES SECTION
+slides.forEach((image, i) =>{
+  nextKey.addEventListener('click', ()=>{
+    image.style.display = 'inline';
+    // image.style.display = 'none';
+  });
+
 })
