1c7a81e2e8d861a4fcbf33b134ab4b87866ae45a
Abdulfatai Quadri Adewale <75619825+abujafar93@users.noreply.github.com>
2022-06-30 12:19:59 +0100

    create gallery

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const hambDropDownDetails = document.querySelector('.list');
 const slides = document.querySelector('.slideImages')
 const previousKey = document.querySelector('#previous');
 const nextKey = document.querySelector('#next');
+let productDisplay = document.querySelector('.displayImage');
+let product1 = document.querySelector('#thumb1');
+let product2 = document.querySelector('#thumb2');
+let product3 = document.querySelector('#thumb3');
+let product4 = document.querySelector('#thumb4');
+const decreButton = document.querySelector('.decre img');
+const increButton = document.querySelector('.incre img');
+
 
 
 // HAMBURGER MENU
@@ -23,10 +31,50 @@ hambClose.addEventListener('click', ()=>{
 
 
 // SLIDES SECTION
-slides.forEach((image, i) =>{
-  nextKey.addEventListener('click', ()=>{
-    image.style.display = 'inline';
-    // image.style.display = 'none';
-  });
+// slides.forEach((image, i) =>{
+//   nextKey.addEventListener('click', ()=>{
+//     image.style.display = 'inline';
+//   });
 
+// })
+
+
+// DISPLAY PRODUCTS ON DISPLAY SECTION
+thumb1.addEventListener('click', ()=>{
+  productDisplay.innerHTML = `<img src="images/image-product-1.jpg" alt=""></img>`;
+  console.log(productDisplay)
+})
+
+thumb2.addEventListener('click', ()=>{
+  productDisplay.innerHTML = `<img src="images/image-product-2.jpg" alt=""></img>`;
+  console.log(productDisplay)
+})
+
+thumb3.addEventListener('click', ()=>{
+  productDisplay.innerHTML = `<img src="images/image-product-3.jpg" alt=""></img>`;
+  console.log(productDisplay)
+})
+
+thumb4.addEventListener('click', ()=>{
+  productDisplay.innerHTML = `<img src="images/image-product-4.jpg" alt=""></img>`;
+  console.log(productDisplay)
+})
+
+// CART PRODUCT QUANTITY
+increButton.addEventListener('click', ()=>{
+  let quantityDisplay = document.querySelector('.quantity h4');
+  quantityDisplay.innerHTML = 0;
+ for(i=0; i <= 30; i++){
+  quantityDisplay.innerHTML = i;
+  console.log(quantityDisplay.innerHTML);
+ }
+})
+
+decreButton.addEventListener('click', ()=>{
+  let quantityDisplay = document.querySelector('.quantity h4');
+  quantityDisplay.innerHTML = 0;
+ for(i; i >= 0; i--){
+  quantityDisplay.innerHTML = i;
+  console.log(quantityDisplay.innerHTML);
+ }  
 })
